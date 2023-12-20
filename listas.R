a2337ab3adf0a211d3e02cc7182ae66a474f2e57
Rafael Brendo <rafalive122@hotmail.com>
2023-12-20 15:31:44 -0300

    Create listas.R

---
+++ listas.R
@@ -0,0 +1,15 @@
+#Listas list()
+
+lista1 <- list("a", 1L, 1.5, TRUE)
+lista1
+typeof(lista1)
+
+lista2 <- list(list(list(1 , 3, 5)))
+lista2
+
+str(lista2)
+str(lista1)
+
+#Nomeando os elementos de uma lista
+lista_nomeada <- list('Chicago' = 1, 'New York' = 2, 'Los Angeles' = 3)
+lista_nomeada
