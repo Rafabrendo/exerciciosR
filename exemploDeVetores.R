63fe5ff5b1b770f28b85dda5cb65e72b510539ab
Rafael Brendo <rafalive122@hotmail.com>
2023-12-20 14:55:32 -0300

    vetores

---
+++ exemploDeVetores.R
@@ -0,0 +1,27 @@
+?print() #para mostrar o que essa função faz
+#Here's an example of a variable
+first_variable <- "This is my first variable"
+print(first_variable)
+secont_variable <- 12.5
+vec_1 <- c(13, 48.5, 71, 101.5, 2)
+vec_1
+x <- c(1, 2, 3)
+names(x) <- c("um","dois","tres")
+x
+#Criando vetor de números inteiros
+numeros_inteiros <- c(1L, 5L, 15L)
+numeros_inteiros
+#Vetor de caracteres 
+vec_caracteres <- c("Sara", "Rafa", "Lisa")
+#Vetor lógico
+vec_logico <- c(TRUE, FALSE, TRUE)
+vec_logico
+typeof(vec_logico)
+typeof(vec_caracteres)
+typeof(numeros_inteiros)
+typeof(vec_1)
+is.logical(numeros_inteiros)
+is.double(vec_caracteres)
+is.integer(numeros_inteiros)
+is.character(vec_caracteres)
+is.logical(vec_logico)
