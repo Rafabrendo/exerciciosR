32aaa48fefb0f1fbd98c5b848fb56d38d0c0fb18
Rafael Brendo <rafalive122@hotmail.com>
2024-01-04 12:09:37 -0300

    Update organizandoOsDados.R

--- organizandoOsDados.R
+++ organizandoOsDados.R
@@ -8,3 +8,11 @@ penguins %>% arrange(- bill_length_mm)
 pinguins2 <- penguins %>% arrange(- bill_length_mm)
 
 View(pinguins2)
+
+penguins %>% group_by(island) %>% drop_na() %>% summarize(mean_bill_length_mm = mean(bill_length_mm))
+
+penguins %>%  group_by(island) %>%  drop_na() %>% summarize(max_bill_length_mm = max(bill_length_mm))
+
+penguins %>% group_by(species, island) %>% drop_na() %>% summarize(max_bl = max(bill_length_mm), mean_bl = mean(bill_length_mm))
+
+penguins %>%  filter(species == "Adelie")
